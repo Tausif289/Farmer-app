3937d94e899c5108b124e5772ebd507030e92de9
tausif289 <ansartausif991@gmail.com>
2025-11-05 09:31:26 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app=express()
 app.use(express.json({ limit: "50mb" }));
 app.use(cors({
     origin: [
-    "http://localhost:5173", // ✅ Local development
+    "http://localhost:5173",               // ✅ Local development
     "https://farmer-app-6iz6.onrender.com" // ✅ Deployed frontend on Render
   ], // or "*" to allow all origins
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
