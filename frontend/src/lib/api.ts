fb03e27009c6ea3af20648d972ba8f8695630d00
tausif289 <ansartausif991@gmail.com>
2025-11-09 14:58:16 +0530

    modifiedlink

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -21,7 +21,7 @@ interface Feedback {
   updatedAt: string;
 }
 
-export const API_BASE ="http://localhost:4000/api";
+export const API_BASE ="https://farmer-app-backend-ocin.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE,
