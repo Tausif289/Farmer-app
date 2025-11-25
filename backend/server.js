7b4411377d0ccf2b5f1a477461f4952fad24669c
tausif ansar <ansartausif991@gmail.com>
2025-11-25 18:14:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,8 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cors({
     origin: [
     "http://localhost:5173",               // ✅ Local development
-    "https://farmer-app-6iz6.onrender.com" // ✅ Deployed frontend on Render
+    "https://farmer-app-6iz6.onrender.com" ,
+    "https://farmeradvisory.vercel.app"// ✅ Deployed frontend on Render
   ], // or "*" to allow all origins
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true,
