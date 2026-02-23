431ed6ef118def981157686454c2ee828c72260f
tausif ansar <ansartausif991@gmail.com>
2026-02-23 14:15:23 +0530

    Update CORS origin for production server

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cors({
     origin: [
     "http://localhost:5173",              
-    "https://farmer-app-6iz6.onrender.com" ,
+    "https://farmer-bl1m.onrender.com" ,
     "https://farmeradvisory.vercel.app"
   ], // or "*" to allow all origins
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
